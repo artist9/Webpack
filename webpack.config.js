e52a39ec4ec7536bd0d55bb6fc9c9f058139b5bb
Artist <krlmnhzn@gmail.com>
2023-06-11 19:24:06 +0300

    11.06.23.home

--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,33 @@ module.exports = {
 			template: path.resolve(__dirname, "src/index.html")
 	}),
 		new CleanWebpackPlugin()
-	]
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.less$/i,
+				use: [
+					// compiles Less to CSS
+					"style-loader",
+					"css-loader",
+					"less-loader",
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					"style-loader",
+					// Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
+		],
+	},
 }
